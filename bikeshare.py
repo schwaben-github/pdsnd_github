1e4c23ca4f27bc30d591f63c71b877a83f2d7a0a
schwaben-github <schwaben_github@objectmail.com>
2021-01-15 23:20:11 +0100

    refactor: Refactoring production code

--- bikeshare.py
+++ bikeshare.py
@@ -1,6 +1,7 @@
 '''Since this file is empty, because I\'m trying to
 finish the "1. Pre-requisite: Git and Github" from the
-"Front End Developer" path, that's why I\'m adding this
+"Front End Developer" path, and not the "Data Science
+Nanodegree"! that's why I\'m adding this
 multi-line comment to the Python file.'''
 
 print('Hello World')
