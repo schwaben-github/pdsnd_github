2207efc5dc19c06d1f7820c75db39481310cc67c
schwaben-github <schwaben_github@objectmail.com>
2021-01-15 18:39:16 +0100

    docs: Changes to documentation

---
+++ bikeshare.py
@@ -0,0 +1,6 @@
+'''Since this file is empty, because I\'m trying to
+finish the "1. Pre-requisite: Git and Github" from the
+"Front End Developer" path, that's why I\'m adding this
+multi-line comment to the Python file.'''
+
+print('Hello World')
